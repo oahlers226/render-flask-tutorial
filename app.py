e8712f3fdaf650f8e81b8c86107e99f8fb69dc14
Owen Ahlers <owah9532@colorado.edu>
2026-07-26 04:01:30 +0000

    Added test db page

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
 from flask import Flask
+import psycopg2
 app = Flask(__name__)
+
+DATABASE_URL = "postgresql://render_flask_tutorial_db_user:7x58Q5YgWOTMFhPI0GewAICQ04bst1o3@dpg-d9ioasfavr4c73b6ka3g-a/render_flask_tutorial_db"
 
 @app.route('/')
 def hello_world():
     return 'Hello World from Owen Ahlers in 3308'
+
+
+@app.route("/db_test")
+def db_test():
+    conn = None
+    try:
+        conn = psycopg2.connect(DATABASE_URL)
+        return "Database connection successful"
+    except Exception as e:
+        return f"Database connection failed: {e}"
+    finally:
+        if conn is not None:
+            conn.close()
